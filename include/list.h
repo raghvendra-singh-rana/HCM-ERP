bcd7923369136f6874b1f7e64958d0fbb53e1911
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:22:24 -0500

    updated list

--- include/list.h
+++ include/list.h
@@ -1,3 +1,11 @@
+/** @file list.h
+ *  @brief Function prototypes for list
+ *
+ *  
+ * This header file is used to display 
+ * all the employees to admin and is present in admin menu
+ *
+ */
 #ifndef LIST_H
 #define LIST_H
 
