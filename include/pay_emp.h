2e0527b5054aafbf7233d8b9b57a89cc62be10e8
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:43:10 -0500

    updated pay_emp

--- include/pay_emp.h
+++ include/pay_emp.h
@@ -1,3 +1,12 @@
+/** @file pay_emp.h
+ *  @brief Function prototypes for pay_emp
+ *
+ *  
+ * This header fileis used to display 
+ * the pay stub of the employee and is
+ * present in employee menu
+ *
+ */
 #ifndef pay_emp_H
 #define pay_emp_H
 
