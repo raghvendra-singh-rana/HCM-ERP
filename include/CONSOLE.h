bb71ae0ea1022f10f3f2e5b6521622dfcbb6bf8e
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 00:19:30 -0500

    console.h updated

--- include/CONSOLE.h
+++ include/CONSOLE.h
@@ -1,3 +1,11 @@
+/** @file console.h
+ *  @brief Function prototypes for console
+ *
+ *  
+ * This function is used to provide the coordinates 
+ * where we want to display the information
+ *
+ */
 #ifndef console_H
 #define console_H
 
