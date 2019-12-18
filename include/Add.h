22ea82366837130658381c98eea594a49be3dda1
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 00:17:54 -0500

    updated add

--- include/Add.h
+++ include/Add.h
@@ -1,3 +1,11 @@
+/** @file add.h
+ *  @brief Function prototypes for add
+ *
+ *  
+ * This function is used to add the employee and 
+ * is used in admin menu only
+ *
+ */
 #ifndef add_H
 #define add_H
 
