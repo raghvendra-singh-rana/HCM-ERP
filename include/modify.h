99565268750ba7e4ac36aa7960c9a516f6611119
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:33:17 -0500

    updated modify

--- include/modify.h
+++ include/modify.h
@@ -1,3 +1,11 @@
+/** @file modify.h
+ *  @brief Function prototypes for modify
+ *
+ *  
+ * This header file is used to modify the 
+ * employee record by admin and us present in admon menu
+ *
+ */
 #ifndef modify_H
 #define modify_H
 
