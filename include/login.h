9df7a4e5b53c4decfcb70561b484c1a3ddc0cddf
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:25:28 -0500

    updated login

--- include/login.h
+++ include/login.h
@@ -1,3 +1,13 @@
+/** @file login.h
+ *  @brief Function prototypes for login
+ *
+ *  
+ * This header file is used to verify 
+ * the login credentials and according to the 
+ * entered credentials open admin menu or employee menu
+ *
+ *
+ */
 #ifndef login_H
 #define login_H
 
