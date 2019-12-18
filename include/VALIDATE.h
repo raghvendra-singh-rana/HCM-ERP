c84900f1a36a6b87682b87dbdcb326643c9b2344
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:07:56 -0500

    updated validate

--- include/VALIDATE.h
+++ include/VALIDATE.h
@@ -1,3 +1,12 @@
+/** @file validate.h
+ *  @brief Function prototypes for validate
+ *
+ *  
+ * This function is used to validate the values entered
+ * by the user if the user enters wrong value it will display
+ *error related to the mistake
+ *
+ */
 #ifndef validate_H
 #define validate_H
 
