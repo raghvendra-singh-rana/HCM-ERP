6a865f8d728e803234796611c4089b98932226c9
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:18:08 -0500

    update delete

--- include/delete.h
+++ include/delete.h
@@ -1,3 +1,11 @@
+/** @file delete.h
+ *  @brief Function prototypes for delete
+ *
+ *  
+ * This header file is used to delete
+ * the employee record and is used in admin menu
+ *
+ */
 #ifndef delete_H
 #define delete_H
 
