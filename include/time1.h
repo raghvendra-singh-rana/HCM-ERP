a92f76c8c51919c7a71d2edf4b12170c88eb191a
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:56:08 -0500

    updated time1

--- include/time1.h
+++ include/time1.h
@@ -1,3 +1,11 @@
+/** @file time1.h
+ *  @brief Function prototypes for time1
+ *
+ *  
+ * This header file has a function is used to display current time 
+ * 
+ *
+ */
 #ifndef time1_H
 #define time1_H
 
