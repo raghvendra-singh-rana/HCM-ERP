bafcae1daa88df8ec72bdfb1b67c152ebdfb5820
abhi16de <56750424+abhi16de@users.noreply.github.com>
2019-12-18 18:57:57 -0500

    updated user_name

--- include/user_name.h
+++ include/user_name.h
@@ -1,3 +1,11 @@
+/** @file user_name.h
+ *  @brief Function prototypes for user_name
+ *
+ *  
+ * This header file has a function is used to  
+ * display the user_name on the screen
+ *
+ */
 #ifndef user_name_H
 #define user_name_H
 
